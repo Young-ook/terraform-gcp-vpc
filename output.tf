09078e6e503f9f3b953feba400d36c0d4e736cb4
Young <youngook.kim@samsung.com>
2018-01-15 19:29:50 +0000

    add map of create subnets to output

--- output.tf
+++ output.tf
@@ -12,3 +12,13 @@ output "vpc_gateway_ip4v" {
   description = "IP address of gateway for this VPC"
   value       = "${google_compute_network.vpc.gateway_ipv4}"
 }
+
+output "private_subnets_map" {
+  description = "Map output of private subnets"
+  value       = "${zipmap(var.regions, google_compute_subnetwork.private.*.self_link)}"
+}
+
+output "public_subnets_map" {
+  description = "Map output of public subnets"
+  value       = "${zipmap(var.regions, google_compute_subnetwork.public.*.self_link)}"
+}
