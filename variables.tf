741e0442968f21742d6eab412445122ac038d67b
Young <youngook.kim@samsung.com>
2018-01-17 23:22:44 +0000

    add project variable

--- variables.tf
+++ variables.tf
@@ -1,10 +1,18 @@
 variable "vpc_name" {
   description = "VPC name"
+  type        = "string"
   default     = "default"
 }
 
 variable "vpc_desc" {
-  description = "Information of this VPC"
+  description = "Information of this vpc"
+  type        = "string"
+  default     = ""
+}
+
+variable "project" {
+  description = "Project ID of gcp (if it's not provided, porject is defined in provider will be used)"
+  type        = "string"
   default     = ""
 }
 
